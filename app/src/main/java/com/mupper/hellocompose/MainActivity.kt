d2524ab079f62ce31e47eba06e1bbc7faf7d000f
jesus-medina <jesus.daniel.medina.cruz@gmail.com>
2022-01-10 15:53:10 -0800

    Componentes principales y layouts

--- app/src/main/java/com/mupper/hellocompose/MainActivity.kt
+++ app/src/main/java/com/mupper/hellocompose/MainActivity.kt
@@ -3,10 +3,14 @@ package com.mupper.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.mupper.hellocompose.ui.theme.HelloComposeTheme
 
@@ -21,7 +25,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HelloCompose() {
-    Text("Hello Compose :(")
+    Column {
+        Image(painterResource(id = R.drawable.platzi), contentDescription = null)
+        Text("Este curso es el mejor!!")
+        Text("Curso de Jetpack compose")
+        Button(onClick = {
+            
+        }) {
+            Text("Click me")
+        }
+    }
 }
 
 @Preview(
