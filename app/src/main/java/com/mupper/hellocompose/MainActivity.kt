efbb2dd74a07dc0fae835610ace28d3232c95042
jesus-medina <jesus.daniel.medina.cruz@gmail.com>
2022-01-10 17:25:47 -0800

    Componentes personalizados y modifiers

--- app/src/main/java/com/mupper/hellocompose/MainActivity.kt
+++ app/src/main/java/com/mupper/hellocompose/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mupper.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,14 +28,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HelloCompose() {
-    Column {
-        Image(painterResource(id = R.drawable.platzi), contentDescription = null)
-        Text("Este curso es el mejor!!")
-        Text("Curso de Jetpack compose")
-        Button(onClick = {
-            
-        }) {
-            Text("Click me")
+    Card(
+        elevation = 4.dp,
+        shape = RoundedCornerShape(20.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource(id = R.drawable.platzi),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp, 40.dp)
+            )
+            Text(
+                "Este curso es el mejor!!",
+                style = MaterialTheme.typography.h4
+            )
+            Text("Curso de Jetpack compose")
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text("Click me")
+            }
         }
     }
 }
